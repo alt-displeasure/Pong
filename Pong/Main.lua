32a03a80d9d048b74d046a958d71f70b62c2003e
Sofia <sofiamoramaldonado@gmail.com>
2022-09-17 19:44:11 -0300

    Rectangles drawn

--- Pong/Main.lua
+++ Pong/Main.lua
@@ -8,21 +8,25 @@ VIRTUAL_HEIGHT = 243
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
-        vsync = true
+        vsync = true,
     })
 end
 
 function love.draw()
     push:apply('start')
-        love.graphics.printf(
-            'Hello Pong!',          
-            0,                      
-            VIRTUAL_HEIGHT / 2 - 6, 
-            VIRTUAL_WIDTH,          
-            'center')
+
+    love.graphics.clear(40/225, 45/225, 52/225, 255)
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, VIRTUAL_HEIGHT-50,5,20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
+
     push:apply('end')           
 end
 
