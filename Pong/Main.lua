bd46b44dc8c8f5f9eb058c185f6c3690adb53404
Sofia <sofiamoramaldonado@gmail.com>
2022-09-19 14:47:52 -0300

    ball motion

--- Pong/Main.lua
+++ Pong/Main.lua
@@ -11,6 +11,8 @@ PADDLE_SPEED = 200
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    math.randomseed(os.time())
+
     smallFont = love.graphics.newFont('font.ttf', 8)
     scoreFont = love.graphics.newFont('font.ttf',32)
 
@@ -27,19 +29,33 @@ function love.load()
 
     paddle1 = 30
     paddle2 = VIRTUAL_HEIGHT - 50
+
+    ballX = VIRTUAL_WIDTH / 2 - 3
+    ballY = VIRTUAL_HEIGHT / 2 - 3
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
+    gameState='start'
 end
 
 function love.update(dt)
     if love.keyboard.isDown('w') then
-        paddle1 = paddle1 - dt*PADDLE_SPEED
+        paddle1 = math.max(0, paddle1 - dt*PADDLE_SPEED)
+
     elseif love.keyboard.isDown('s') then
-        paddle1 = paddle1 + dt*PADDLE_SPEED
+        paddle1 = math.min( VIRTUAL_HEIGHT-26, paddle1 + dt*PADDLE_SPEED)
     end
 
     if love.keyboard.isDown('up') then
-        paddle2 = paddle2 - dt*PADDLE_SPEED
+        paddle2 = math.max(0, paddle2 - dt*PADDLE_SPEED)
     elseif love.keyboard.isDown('down') then
-        paddle2 = paddle2 + dt*PADDLE_SPEED
+        paddle2 = math.min(VIRTUAL_HEIGHT-26, paddle2 + dt*PADDLE_SPEED)
+    end
+
+    if gameState == 'play' then
+        ballX = ballX + ballDX*dt
+        ballY = ballY + ballDY*dt
     end
 end
 
@@ -51,9 +67,9 @@ function love.draw()
     love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.rectangle('fill', 10, paddle1, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, paddle2, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
+    love.graphics.rectangle('fill', 10, paddle1, 5, 25)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, paddle2, 5, 25)
+    love.graphics.rectangle('fill', ballX, ballY, 6, 6)
 
     love.graphics.setFont(scoreFont)
 
@@ -68,5 +84,17 @@ end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ballX = VIRTUAL_WIDTH / 2 - 3
+            ballY = VIRTUAL_HEIGHT / 2 - 3
+
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
     end
 end
